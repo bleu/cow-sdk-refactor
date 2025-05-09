8303e621167858b30656fed44b9c53d604679f45
Jean Neiverth <jeanneiverth@gmail.com>
2025-05-09 14:33:11 -0300

    chore: add type test

--- packages/ui/src/test.ts
+++ packages/ui/src/test.ts
@@ -1,8 +1,18 @@
+import { ContractsTs } from '@cowprotocol/contracts-ts'
+
 import { JsonRpcProvider } from '@ethersproject/providers'
 import { EthersV5Adapter } from '@cowprotocol/sdk-ethers-v5-adapter'
-import { ContractsTs } from '@cowprotocol/contracts-ts'
+
+import { http } from 'viem'
+import { mainnet } from 'viem/chains'
+import { ViemAdapter } from '@cowprotocol/sdk-viem-adapter'
 
 const adapter = new EthersV5Adapter(new JsonRpcProvider())
 const contracts = new ContractsTs(adapter)
 
-export const x = contracts
+const adapterViem = new ViemAdapter(mainnet, http())
+const contractsViem = new ContractsTs(adapterViem)
+
+// export const x = contracts.hashTypedData()
+
+// export const y = contractsViem.hashTypedData()
