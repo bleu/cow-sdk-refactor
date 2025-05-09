4bfbf9ce29bd41487ec4291d4a2ab54ebee0db1b
Jean Neiverth <jeanneiverth@gmail.com>
2025-05-09 10:18:06 -0300

    feat: add interaction file to contracts-ts

--- packages/contracts-ts/src/ContractsTs.ts
+++ packages/contracts-ts/src/ContractsTs.ts
@@ -1,5 +1,6 @@
 import { AbstractProviderAdapter, AdapterTypes } from '@cowprotocol/common'
 import { DeploymentArguments, MaybeNamedArtifactArtifactDeployment } from './deploy'
+import { Interaction, InteractionLike, normalizeInteraction, normalizeInteractions } from './interaction'
 
 export class ContractsTs<T extends AdapterTypes = AdapterTypes> {
   SALT: string
@@ -27,6 +28,29 @@ export class ContractsTs<T extends AdapterTypes = AdapterTypes> {
     const deployData = adapter.hexConcat([bytecode, this.adapter.encodeDeploy(deploymentArguments, abi as T['Abi'])])
 
     return adapter.getCreate2Address(this.DEPLOYER_CONTRACT, this.SALT, this.adapter.keccak256(deployData))
+  }
+
+  /**
+   * Normalizes interaction data so that it is ready to be be ABI encoded.
+   *
+   * @param interaction The interaction to normalize.
+   * @return The normalized interaction.
+   */
+  public normalizeInteraction(
+    interaction: InteractionLike<T['BigIntish'], T['Bytes']>,
+  ): Interaction<T['BigIntish'], T['Bytes']> {
+    return normalizeInteraction<T['BigIntish'], T['Bytes']>(interaction)
+  }
+
+  /**
+   * Normalizes data for many interactions so that they can be ABI encoded. This
+   * calls [`normalizeInteraction`] for each interaction.
+   *
+   * @param interactions The interactions to normalize.
+   * @return The normalized interactions.
+   */
+  public normalizeInteractions(interactions: InteractionLike<T['BigIntish'], T['Bytes']>[]): Interaction[] {
+    return normalizeInteractions<T['BigIntish'], T['Bytes']>(interactions)
   }
 
   // ... other methods from contracts-ts that need adapter
--- packages/contracts-ts/src/interaction.ts
+++ packages/contracts-ts/src/interaction.ts
@@ -1,51 +1,49 @@
-// import type { BigNumberish, BytesLike } from "ethers";
+/**
+ * Gnosis Protocol v2 interaction data.
+ */
+export interface Interaction<BigIntish = unknown, Bytes = unknown> {
+  /**
+   * Address of the smart contract to be called in this interaction.
+   */
+  target: string
+  /**
+   * Call value in wei for the interaction, allowing Ether to be sent.
+   */
+  value: BigIntish
+  /**
+   * Call data used in the interaction with a smart contract.
+   */
+  callData: Bytes
+}
 
-// /**
-//  * Gnosis Protocol v2 interaction data.
-//  */
-// export interface Interaction {
-//   /**
-//    * Address of the smart contract to be called in this interaction.
-//    */
-//   target: string;
-//   /**
-//    * Call value in wei for the interaction, allowing Ether to be sent.
-//    */
-//   value: BigNumberish;
-//   /**
-//    * Call data used in the interaction with a smart contract.
-//    */
-//   callData: BytesLike;
-// }
+export type InteractionLike<BigIntish, Bytes> = Pick<Interaction<BigIntish, Bytes>, 'target'> &
+  Partial<Interaction<BigIntish, Bytes>>
 
-// export type InteractionLike = Pick<Interaction, "target"> &
-//   Partial<Interaction>;
+/**
+ * Normalizes interaction data so that it is ready to be be ABI encoded.
+ *
+ * @param interaction The interaction to normalize.
+ * @return The normalized interaction.
+ */
+export function normalizeInteraction<BigIntish, Bytes>(
+  interaction: InteractionLike<BigIntish, Bytes>,
+): Interaction<BigIntish, Bytes> {
+  return {
+    value: 0 as BigIntish,
+    callData: '0x' as Bytes,
+    ...interaction,
+  }
+}
 
-// /**
-//  * Normalizes interaction data so that it is ready to be be ABI encoded.
-//  *
-//  * @param interaction The interaction to normalize.
-//  * @return The normalized interaction.
-//  */
-// export function normalizeInteraction(
-//   interaction: InteractionLike,
-// ): Interaction {
-//   return {
-//     value: 0,
-//     callData: "0x",
-//     ...interaction,
-//   };
-// }
-
-// /**
-//  * Normalizes data for many interactions so that they can be ABI encoded. This
-//  * calls [`normalizeInteraction`] for each interaction.
-//  *
-//  * @param interactions The interactions to normalize.
-//  * @return The normalized interactions.
-//  */
-// export function normalizeInteractions(
-//   interactions: InteractionLike[],
-// ): Interaction[] {
-//   return interactions.map(normalizeInteraction);
-// }
+/**
+ * Normalizes data for many interactions so that they can be ABI encoded. This
+ * calls [`normalizeInteraction`] for each interaction.
+ *
+ * @param interactions The interactions to normalize.
+ * @return The normalized interactions.
+ */
+export function normalizeInteractions<BigIntish, Bytes>(
+  interactions: InteractionLike<BigIntish, Bytes>[],
+): Interaction[] {
+  return interactions.map(normalizeInteraction)
+}
